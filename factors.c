49879ce342f9c723cc277c8e9325819ae09122f7
Rx Chris Ameh <chris1jnr@gmail.com>
2024-10-05 10:57:17 -0700

    factors

--- factors.c
+++ factors.c
@@ -1,29 +1,46 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
-/*
-    Finds the smallest divisor, if any, of a given number `n`
-    Returns:
-        smallest divisor if found
-        0 if n is prime
-*/
-int trial_division(long int n){
-long int f;
+#include <math.h>
+#include <string.h>
 
-if (n%2 == 0){
-	printf("%lu=%lu*%i\n", n, n/2, 2);
-	return 0;
-}
-
-f = 3;
-while (f*f <= n)
+int main(int argc, char *argv[])
 {
-	if (n%f == 0){
-		printf("%lu=%lu*%lu\n", n, n/f, f);
-		return 0;
+	FILE *stream;
+	char *line = NULL;
+	size_t len = 0;
+	long long flag = 1, div, rest, number, counter;
+	ssize_t nread;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
-	else{
-		f += 2;
+
+	stream = fopen(argv[1], "r");
+	if (stream == NULL) {
+		perror("fopen");
+		exit(EXIT_FAILURE);
 	}
-}
-printf("%lu=%lu*%i\n", n, n, 1);
-return 0;
+
+	while ((nread = getline(&line, &len, stream)) != -1) {
+		flag = 1, div = 2;
+		number = atoll(line);
+		while (flag) {
+			rest = number % div;
+			if (!rest) {
+				counter = number / div;
+				printf("%lld=%lld*%lld\n", number, counter, div);
+				flag = 0;
+			}
+			div++;
+		}
+	}
+
+	free(line);
+	fclose(stream);
+	exit(EXIT_SUCCESS);
 }
